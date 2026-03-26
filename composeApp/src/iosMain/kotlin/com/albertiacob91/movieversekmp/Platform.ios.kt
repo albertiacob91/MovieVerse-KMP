168beb1c8d4dbe908c70086c9b8f6861ac1b11bc
Albert Iacob <159893365+alb3rt91@users.noreply.github.com>
2026-03-26 21:23:09 +0100

    feat: create initial Kotlin Multiplatform project

---
+++ composeApp/src/iosMain/kotlin/com/albertiacob91/movieversekmp/Platform.ios.kt
@@ -0,0 +1,9 @@
+package com.albertiacob91.movieversekmp
+
+import platform.UIKit.UIDevice
+
+class IOSPlatform: Platform {
+    override val name: String = UIDevice.currentDevice.systemName() + " " + UIDevice.currentDevice.systemVersion
+}
+
+actual fun getPlatform(): Platform = IOSPlatform()
---
+++ composeApp/src/jvmMain/kotlin/com/albertiacob91/movieversekmp/Platform.jvm.kt
@@ -0,0 +1,7 @@
+package com.albertiacob91.movieversekmp
+
+class JVMPlatform: Platform {
+    override val name: String = "Java ${System.getProperty("java.version")}"
+}
+
+actual fun getPlatform(): Platform = JVMPlatform()
