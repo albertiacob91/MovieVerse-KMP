1528a6e47a1d3b59c1cec448d2b70e6b140e0907
Albert Luis Iacob Istrati <albeertiacob@gmail.com>
2026-03-29 20:00:40 +0200

    refactor: clean template and add base app entry point (#3)

--- composeApp/src/commonMain/kotlin/com/albertiacob91/movieversekmp/Greeting.kt
+++
@@ -1,9 +0,0 @@
-package com.albertiacob91.movieversekmp
-
-class Greeting {
-    private val platform = getPlatform()
-
-    fun greet(): String {
-        return "Hello, ${platform.name}!"
-    }
-}
