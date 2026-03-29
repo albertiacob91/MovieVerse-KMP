2c11e0045b35fc37f66853920716a3e73595d655
Albert Luis Iacob Istrati <albeertiacob@gmail.com>
2026-03-29 20:03:16 +0200

    refactor: organize presentation layer structure

---
+++ composeApp/src/commonMain/kotlin/com/albertiacob91/movieversekmp/presentation/screens/HomeScreen.kt
@@ -0,0 +1,9 @@
+package com.albertiacob91.movieversekmp.presentation.screens
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun HomeScreen() {
+    Text("MovieVerse Home")
+}
