0215532c25a6fed9edbfca50fcee826f695e1e24
Albert Luis Iacob Istrati <albeertiacob@gmail.com>
2026-03-30 22:10:50 +0200

    feat: add backend base with postgres connection (#5)

---
+++ server/src/main/kotlin/com/albertiacob91/movieversekmp/server/Application.kt
@@ -0,0 +1,33 @@
+package com.albertiacob91.movieversekmp.server
+
+import com.albertiacob91.movieversekmp.server.config.DatabaseFactory
+import io.ktor.serialization.kotlinx.json.*
+import io.ktor.server.application.*
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
+import io.ktor.server.plugins.contentnegotiation.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+
+fun main() {
+    embeddedServer(Netty, port = 8081, host = "0.0.0.0", module = Application::module)
+        .start(wait = true)
+}
+
+fun Application.module() {
+    DatabaseFactory.init()
+
+    install(ContentNegotiation) {
+        json()
+    }
+
+    routing {
+        get("/") {
+            call.respond(mapOf("message" to "MovieVerse server running"))
+        }
+
+        get("/health") {
+            call.respond(mapOf("status" to "ok"))
+        }
+    }
+}
