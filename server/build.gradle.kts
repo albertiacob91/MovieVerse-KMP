b8457aa0fccc365a60c27266e4501106631d4534
Albert Luis Iacob Istrati <albeertiacob@gmail.com>
2026-03-30 22:08:12 +0200

    feat: add backend base with postgres connection

---
+++ server/build.gradle.kts
@@ -0,0 +1,35 @@
+plugins {
+    kotlin("jvm")
+    id("io.ktor.plugin") version "3.3.1"
+    application
+}
+
+group = "com.albertiacob91.movieversekmp"
+version = "0.1.0"
+
+repositories {
+    mavenCentral()
+    google()
+}
+
+application {
+    mainClass.set("com.albertiacob91.movieversekmp.server.ApplicationKt")
+}
+
+dependencies {
+    implementation("io.ktor:ktor-server-core:3.3.1")
+    implementation("io.ktor:ktor-server-netty:3.3.1")
+    implementation("io.ktor:ktor-server-content-negotiation:3.3.1")
+    implementation("io.ktor:ktor-serialization-kotlinx-json:3.3.1")
+    implementation("io.ktor:ktor-server-status-pages:3.3.1")
+    implementation("ch.qos.logback:logback-classic:1.5.18")
+
+    testImplementation("io.ktor:ktor-server-test-host:3.3.1")
+    testImplementation(kotlin("test"))
+
+    implementation("org.jetbrains.exposed:exposed-core:0.61.0")
+    implementation("org.jetbrains.exposed:exposed-jdbc:0.61.0")
+    implementation("org.jetbrains.exposed:exposed-dao:0.61.0")
+    implementation("com.zaxxer:HikariCP:6.3.0")
+    implementation("org.postgresql:postgresql:42.7.7")
+}
